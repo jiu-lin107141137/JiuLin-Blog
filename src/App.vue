7450d21f69b907a2d08bcb87372ab155e8868acc
jiu-lin <jam930725@gmail.com>
2023-04-23 18:11:18 +0800

    update article preview demo

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,30 @@
 import NavBar from '@/components/Navbar.vue';
 import BgComp from '@/components/BackgroungComp.vue';
 import FooterComp from '@/components/FooterComp.vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
+
+const windowWidth = ref(0);
+const onWindowSizeChanged = () => windowWidth.value = window.innerWidth;
+
+onMounted(() => {
+  windowWidth.value = window.innerWidth;
+  window.addEventListener('resize', onWindowSizeChanged);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', onWindowSizeChanged);
+})
+
+const isPhoneScreen = computed(() => windowWidth.value < 576);
+
 </script>
 
 <template>
   <NavBar />
   <router-view />
   <FooterComp />
-  <BgComp />
+  <BgComp v-if="!isPhoneScreen"/>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
